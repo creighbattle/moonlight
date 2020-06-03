1639eb5438d817c876a2b5494222eb88bd100510
Creigh Battle <creighbattle@Creighs-MacBook-Air.local>
2020-06-03 18:40:14 -0400

    Updates

---
+++ precache-manifest.4b05eb60d07c38ef7e9ddeaa7b2e386f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "240925a9d6ad6ea575edf5cdc245367b",
+    "url": "/moonlight/index.html"
+  },
+  {
+    "revision": "694f00d066635dfb94b2",
+    "url": "/moonlight/static/css/2.5e919c6e.chunk.css"
+  },
+  {
+    "revision": "30058555dd898c9f4405",
+    "url": "/moonlight/static/css/main.1e09a95a.chunk.css"
+  },
+  {
+    "revision": "694f00d066635dfb94b2",
+    "url": "/moonlight/static/js/2.74212c63.chunk.js"
+  },
+  {
+    "revision": "a855a7222d00a33eb6846ce89bcfdce5",
+    "url": "/moonlight/static/js/2.74212c63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30058555dd898c9f4405",
+    "url": "/moonlight/static/js/main.3b6db6ae.chunk.js"
+  },
+  {
+    "revision": "f25e0e4331b7d2eaff9e",
+    "url": "/moonlight/static/js/runtime-main.c204bdf1.js"
+  },
+  {
+    "revision": "1c12d2e6828c5a9fb8db8e39c998c246",
+    "url": "/moonlight/static/media/creighbattle.1c12d2e6.jpg"
+  },
+  {
+    "revision": "9e9398235ceabf213c282838837620d3",
+    "url": "/moonlight/static/media/outdoors.9e939823.jpg"
+  },
+  {
+    "revision": "73262b023c3ff3a8f6e8c24ea7e6a39b",
+    "url": "/moonlight/static/media/palmtrees.73262b02.jpg"
+  }
+]);
